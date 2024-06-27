896191e10434ed7281b3561bdd033b6c92a11f34
Alessio Margelli <alex9446@gmail.com>
2024-06-27 12:26:14 +0200

    catch errors and output to log area

--- parser.js
+++ parser.js
@@ -1,12 +1,23 @@
-function parse() {
-    const input = document.getElementById("input").value
-    const input_split = input.split('Rest Api Logger.INFO:  ')
+function parse(json_string) {
+    const input_split = json_string.split('Rest Api Logger.INFO:  ')
     const input_json = input_split[1].replace(' []', '')
     const input_obj = JSON.parse(input_json)
     input_obj['request']['body'] = JSON.parse(input_obj['request']['body'])
     input_obj['response']['body'] = JSON.parse(input_obj['response']['body'])
-    const output_json = JSON.stringify(input_obj, null, 4)
-    document.getElementById("output").value = output_json
+    return JSON.stringify(input_obj, null, 4)
 }
 
-document.getElementById("btn-parse").addEventListener("click", parse)
+function htmlManager() {
+    const output_elem = document.getElementById("output")
+    const logarea_elem = document.getElementById("logarea")
+    output_elem.value = ''
+    logarea_elem.value = ''
+    try {
+        const parsed_json = parse(document.getElementById("input").value)
+        output_elem.value = parsed_json
+    } catch (error) {
+        logarea_elem.value = error
+    }
+}
+
+document.getElementById("btn-parse").addEventListener("click", htmlManager)
