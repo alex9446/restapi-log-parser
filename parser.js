082cd02f2c39e6d3e09b44b1597f77dabd2bad19
Alessio Margelli <alex9446@gmail.com>
2024-06-27 16:30:40 +0200

    show image if present in json request

--- parser.js
+++ parser.js
@@ -1,20 +1,41 @@
+function parse_img(body_obj) {
+    try {
+        const img = body_obj['entry']['content']
+        const img_base64 = img['base64_encoded_data']
+        const img_type = img['type']
+        return 'data:'+img_type+';base64,'+img_base64
+    } catch (error) {
+        if (!(error instanceof TypeError)) {
+            throw error
+        }
+    }
+    return ''
+}
+
 function parse(json_string) {
     const input_split = json_string.split('Rest Api Logger.INFO:  ')
     const input_json = input_split[1].replace(' []', '')
     const input_obj = JSON.parse(input_json)
     input_obj['request']['body'] = JSON.parse(input_obj['request']['body'])
     input_obj['response']['body'] = JSON.parse(input_obj['response']['body'])
-    return JSON.stringify(input_obj, null, 4)
+    const img_string = parse_img(input_obj['request']['body'])
+    return [JSON.stringify(input_obj, null, 4), img_string]
 }
 
 function htmlManager() {
     const output_elem = document.getElementById("output")
+    const img_elem = document.getElementById("output-img")
     const logarea_elem = document.getElementById("logarea")
     output_elem.value = ''
+    img_elem.style.display = null
     logarea_elem.value = ''
     try {
-        const parsed_json = parse(document.getElementById("input").value)
+        const [parsed_json, img_string] = parse(document.getElementById("input").value)
         output_elem.value = parsed_json
+        if (img_string) {
+            img_elem.style.display = 'block'
+            img_elem.src = img_string
+        }
     } catch (error) {
         logarea_elem.value = error
     }
